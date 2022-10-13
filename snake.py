6023f9ccffa80ae91df4e1f06fc8c060da89cb65
jeeshma-sayone <jeeshma@sayonetech.com>
2022-10-13 18:14:57 +0530

    snake game

--- snake.py
+++ snake.py
@@ -1,50 +1,70 @@
-import pygame
+"""Snake, classic arcade game.
 
-pygame.init()
+Excercises
 
-white = (255, 255, 255)
-black = (0, 0, 0)
-red = (255, 0, 0)
+1. How do you make the snake faster or slower?
+2. How can you make the snake go around the edges?
+3. How would you move the food?
+4. Change the snake to respond to arrow keys.
 
-dis = pygame.display.set_mode((800, 600))
-pygame.display.set_caption('Snake Game by Edureka')
+"""
 
-game_over = False
+from turtle import *
+from random import randrange
+from freegames import square, vector
 
-x1 = 300
-y1 = 300
+food = vector(0, 0)
+snake = [vector(10, 0)]
+aim = vector(0, -10)
 
-x1_change = 0
-y1_change = 0
 
-clock = pygame.time.Clock()
+def change(x, y):
+    "Change snake direction."
+    aim.x = x
+    aim.y = y
 
-while not game_over:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            game_over = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x1_change = -10
-                y1_change = 0
-            elif event.key == pygame.K_RIGHT:
-                x1_change = 10
-                y1_change = 0
-            elif event.key == pygame.K_UP:
-                y1_change = -10
-                x1_change = 0
-            elif event.key == pygame.K_DOWN:
-                y1_change = 10
-                x1_change = 0
 
-    x1 += x1_change
-    y1 += y1_change
-    dis.fill(white)
-    pygame.draw.rect(dis, black, [x1, y1, 10, 10])
+def inside(head):
+    "Return True if head inside boundaries."
+    return -200 < head.x < 190 and -200 < head.y < 190
 
-    pygame.display.update()
 
-    clock.tick(30)
+def move():
+    "Move snake forward one segment."
+    head = snake[-1].copy()
+    head.move(aim)
 
-pygame.quit()
-quit()
+    if not inside(head) or head in snake:
+        square(head.x, head.y, 9, 'red')
+        update()
+        return
+
+    snake.append(head)
+
+    if head == food:
+        print('Snake:', len(snake))
+        food.x = randrange(-15, 15) * 10
+        food.y = randrange(-15, 15) * 10
+    else:
+        snake.pop(0)
+
+    clear()
+
+    for body in snake:
+        square(body.x, body.y, 9, 'black')
+
+    square(food.x, food.y, 9, 'green')
+    update()
+    ontimer(move, 100)
+
+
+setup(420, 420, 370, 0)
+hideturtle()
+tracer(False)
+listen()
+onkey(lambda: change(10, 0), 'Right')
+onkey(lambda: change(-10, 0), 'Left')
+onkey(lambda: change(0, 10), 'Up')
+onkey(lambda: change(0, -10), 'Down')
+move()
+done()
